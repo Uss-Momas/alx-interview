6561c381d0931a766250da1068225a745d2a489a
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-20 14:23:53 +0200

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,26 +13,34 @@ def print_message(size, status_dict):
 
 if __name__ == '__main__':
     try:
-        count = 1
+        count = 0
         size = 0
         statuses_dict = {
-            "200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, 
+            "200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
             "405": 0, "500": 0
         }
         for line in sys.stdin:
-            line_spli = line.split()
-            size += int(line_spli[-1])  # get the size of the current line
-            status = line_spli[-2]  # get the status code of the current line
-            statuses_dict[status] = statuses_dict[status] + 1
-            if count % 10 == 0:
-                print_message(size, statuses_dict)
-                size = 0
-                count = 0
-                statuses_dict = {
-                    "200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
-                    "405": 0, "500": 0
-                }
-            count += 1
+            line_split = line.split()
+
+            if len(line_split) > 2:
+                count += 1
+
+                if count <= 10:
+                    # get the size of the current line
+                    size += int(line_split[-1])
+                    # get the status code of the current line
+                    status = line_split[-2]
+
+                    if status in statuses_dict.keys():
+                        statuses_dict[status] += 1
+
+                if count == 10:
+                    print_message(size, statuses_dict)
+                    count = 0
+                    # statuses_dict = {
+                    #     "200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+                    # "404": 0, "405": 0, "500": 0
+                    # }
 
     except KeyboardInterrupt:
         print_message(size, statuses_dict)
