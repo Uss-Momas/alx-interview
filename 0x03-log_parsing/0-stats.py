25506ec0604df6a94bc1c49138d9e6a6687e27f5
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-20 14:35:09 +0200

    return to another codebase

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,46 @@
 #!/usr/bin/python3
-
+"""0-stats module"""
 import sys
 
 
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+def print_message(size, status_dict):
+    """printing the message well formatted"""
+    print(f"File size: {size}")
+    for key, value in status_dict.items():
+        if value != 0:
+            print(f"{key}: {value}")
 
 
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+if __name__ == '__main__':
+    try:
+        count = 0
+        size = 0
+        statuses_dict = {
+            "200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
+            "405": 0, "500": 0
+        }
+        for line in sys.stdin:
+            line_split = line.split()
 
-try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+            if len(line_split) > 2:
+                count += 1
 
-        if len(parsed_line) > 2:
-            counter += 1
+                if count <= 10:
+                    # get the size of the current line
+                    size += int(line_split[-1])
+                    # get the status code of the current line
+                    status = line_split[-2]
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
+                    if status in statuses_dict.keys():
+                        statuses_dict[status] += 1
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
+                if count == 10:
+                    print_message(size, statuses_dict)
+                    count = 0
+                    # statuses_dict = {
+                    #     "200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+                    # "404": 0, "405": 0, "500": 0
+                    # }
 
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
-finally:
-    print_msg(dict_sc, total_file_size)
+    except KeyboardInterrupt:
+        print_message(size, statuses_dict)
