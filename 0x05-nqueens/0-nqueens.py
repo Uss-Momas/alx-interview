6a05c055d310cd8cab7cad663d1d5dcf96fcdcae
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-11-07 11:48:57 +0200

    Implementation of solution for nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,6 +4,59 @@ implementation of solution for n-queens problem
 """
 import sys
 
+
+def solveNQueens(n):
+    """
+    solveNQueens function implementation
+    """
+    col = set()
+    posDiag = set()  # row + column
+    negativeDiag = set()  # r - c
+    result = []
+    board = [["."] * n for i in range(n)]
+
+    def backtrack(row):
+        """backtrack function"""
+        if row == n:
+            copy = ["".join(r) for r in board]
+            result.append(copy)
+            return
+
+        for c in range(n):
+            if c in col or (row + c) in posDiag or (row - c) in negativeDiag:
+                continue
+            col.add(c)
+            posDiag.add(row + c)
+            negativeDiag.add(row - c)
+            board[row][c] = "Q"
+
+            backtrack(row + 1)
+
+            col.remove(c)
+            posDiag.remove(row + c)
+            negativeDiag.remove(row - c)
+            board[row][c] = "."
+    backtrack(0)
+    return result
+
+
+def printSolution(results):
+    """printSolution - function to print the solution for the NQueens problem
+    it should print a array of positions for every queen in the puzzle
+    Args:
+        - results: a list os lists containing the results in a string format
+    Return:
+        - None
+    """
+    for result in results:
+        index_result = list(zip(range(len(result)), result))
+        queen_positions = [
+            [(i, j) for j, letter in list(zip(range(len(element)), element))
+             if letter == "Q"] for i, element in index_result]
+        positions = [list(element[0]) for element in queen_positions]
+        print(positions)
+
+
 args = sys.argv
 
 if len(args) != 2:
@@ -18,3 +71,5 @@ if not n.isdigit():
 n = int(n)
 if n < 4:
     print("N must be at least 4")
+
+printSolution(solveNQueens(n))
