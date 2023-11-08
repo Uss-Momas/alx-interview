8055b80830ee91bfcc72a786cc3fcd3056ff08f4
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-11-08 13:54:15 +0200

    add packages to ignore

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const api_url = 'https://swapi-api.alx-tools.com/api/';
+
+const process = require('process');
+
+const argv = process.argv;
+
+console.log(argv)
