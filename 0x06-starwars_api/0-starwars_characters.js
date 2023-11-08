acdd887b325949b5359d0c9b6e70d19bc73d8201
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-11-08 14:35:18 +0200

    List in order actors name of a especifi movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,11 +1,29 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-const api_url = 'https://swapi-api.alx-tools.com/api/';
-
-const process = require('process');
+const api_url = 'https://swapi-api.alx-tools.com/api/films';
 
 const argv = process.argv;
 
-console.log(argv)
+request.get(`${api_url}/${argv[2]}`, (err, resp, body) => {
+    if (err) throw err;
+    const movie = JSON.parse(body);
+    const peoples = movie.characters
+
+    // start point for getting peoples name
+    getPeople(peoples, 0);
+});
+
+function getPeople(peoples, position) {
+    // condition of stopping
+    if (position == peoples.length) return;
+
+    request.get(peoples[position], (err, resp, body) => {
+        if (err) throw err;
+
+        const people = JSON.parse(body);
+        console.log(people.name);
+        // get the next actor name
+        getPeople(peoples, position + 1);
+    });
+}
