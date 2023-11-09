b3393c6967c6af5242053f98db811aa8395b3922
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-11-09 10:20:50 +0200

    Fix linter, and make it executable

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,30 @@
 #!/usr/bin/node
+// starwars api calls
 const request = require('request');
 
-const api_url = 'https://swapi-api.alx-tools.com/api/films';
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films';
 
 const argv = process.argv;
 
-request.get(`${api_url}/${argv[2]}`, (err, resp, body) => {
-    if (err) throw err;
-    const movie = JSON.parse(body);
-    const peoples = movie.characters
+request.get(`${apiUrl}/${argv[2]}`, (err, resp, body) => {
+  if (err) throw err;
+  const movie = JSON.parse(body);
+  const peoples = movie.characters;
 
-    // start point for getting peoples name
-    getPeople(peoples, 0);
+  // start point for getting peoples name
+  getPeople(peoples, 0);
 });
 
-function getPeople(peoples, position) {
-    // condition of stopping
-    if (position == peoples.length) return;
+function getPeople (peoples, position) {
+  // condition of stopping
+  if (position === peoples.length) return;
 
-    request.get(peoples[position], (err, resp, body) => {
-        if (err) throw err;
+  request.get(peoples[position], (err, resp, body) => {
+    if (err) throw err;
 
-        const people = JSON.parse(body);
-        console.log(people.name);
-        // get the next actor name
-        getPeople(peoples, position + 1);
-    });
+    const people = JSON.parse(body);
+    console.log(people.name);
+    // get the next actor name
+    getPeople(peoples, position + 1);
+  });
 }
