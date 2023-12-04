a27d269cfed1666fe0d28d551de8efd1684c5a03
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-12-04 10:25:41 +0200

    Add initial file

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""prime_game module
+"""
+
+
+def isWinner(x, nums):
+    """isWinner function
+    Args:
+        - x:  number of rounds
+        - nums:  is an array of n elements
+    Return:
+        - the name of the player who won or None if it's even
+    """
