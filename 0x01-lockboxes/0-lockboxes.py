4c49bb4b5cae886e90d4a940f25f8ff5f700d065
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-02-19 11:14:04 +0200

    feat: implementing BFS for lockboxes problem

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,17 +11,23 @@ def canUnlockAll(boxes):
             - true -> when all can be unlock
             - false -> when we cant
     """
-    size = len(boxes)
-    print(boxes[0])
-    goNext(boxes[0], boxes)
-    return True
-
-def goNext(box, boxes):
-    """test"""
-    if len(box) == 0:
-        return 0
-    next_box = boxes[box.pop(0)]
-    # next_box = boxes[box[0]]
-    # print(box)
-    print(next_box)
-    goNext(next_box, boxes)
+    # Mark as False all the nodes
+    visited = [False] * len(boxes)
+    queue = []
+    # The first Node is 0
+    queue.append(0)
+    # Mark the first node has visited
+    visited[0] = True
+    # count of the number of nodes that are visited during BFS
+    count = 0
+    while len(queue) > 0:
+        current_node = queue.pop(0)
+        count = count + 1
+        for j in boxes[current_node]:
+            # node was not explored
+            if visited[j] is False:
+                queue.append(j)
+                visited[j] = True
+    if count == len(boxes):
+        return True
+    return False
